5f43012cfb25903e8f1621af0d9d19172be24080
ohsem <palieyka@gmail.com>
2021-06-28 00:48:19 +0800

    Update the Authenticate request.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
-const path = require('path')
-const fs = require('fs')
 const axios = require('axios')
 const baseURL = 'https://my-iptv.herokuapp.com'
 const unifiAuthURL = 'https://playtv.unifi.com.my:7042/VSP/V3/Authenticate'
 const unifiEpgURL = 'https://raw.githubusercontent.com/weareblahs/epg/master/unifitv.xml'
+
+const DEFAULT_HEADER = {
+  headers: {
+    Host: 'playtv.unifi.com.my:7042',
+    Origin: 'https://playtv.unifi.com.my'
+  }
+}
 
 module.exports = (fastify, options, next) => {
   fastify.register(require('point-of-view'), {
@@ -18,43 +23,51 @@ module.exports = (fastify, options, next) => {
       epg: [`${baseURL}/epg/unifi-tv`]
     })
   })
-  fastify.get('/m3u8/unifi-tv', ({ query }, res) => {
+  
+  fastify.get('/m3u8/unifi-tv', async ({ query }, res) => {
     const { userID, clientPasswd } = query
-    axios.post(unifiAuthURL, {
-      "authenticateBasic": {
+    const firstAuthData = {
+      authenticateBasic: {
         userID,
         clientPasswd,
-        "userType": "1",
-        "timeZone": "Asia/Brunei",
-        "isSupportWebpImgFormat": "0",
-        "lang": "en"
+        userType: '1',
+        lang: 'en'
+      }
+    }
+    const firstAuth = await axios.post(unifiAuthURL, firstAuthData, DEFAULT_HEADER)
+    const { physicalDeviceID, deviceModel } = firstAuth.data.devices[0]
+    const secondAuthData = {
+      authenticateBasic: {
+        userID,
+        clientPasswd,
+        userType: '1',
+        timeZone: 'Asia/Brunei',
+        isSupportWebpImgFormat: '0',
+        lang: 'en'
       },
-      "authenticateDevice": {
-        "physicalDeviceID": "1928342308",
-        "terminalID": "1928342308",
-        "deviceModel": "PC Web TV"
+      authenticateDevice: {
+        physicalDeviceID,
+        terminalID: physicalDeviceID,
+        deviceModel
       },
-      "authenticateTolerant": {
-        "areaCode": "1200",
-        "templateName": "default",
-        "bossID": "",
-        "userGroup": "-1"
+      authenticateTolerant: {
+        areaCode: '1200',
+        templateName: 'default',
+        bossID: '',
+        userGroup: '-1'
       }
-    }, {
-      headers: {
-        "Host": 'playtv.unifi.com.my:7042',
-        "Origin": 'https://playtv.unifi.com.my'
-      }
-    }).then(({ data }) => {
-      if (data.VUID) {
-        res.view('./src/m3u8/unifi-tv.ejs', { vuid: data.VUID})
-      } else {
-        console.error('Response from unifi server:', data)
-        res.send({ message: 'Something went wrong. Please check your credential again'}, 401)
-      }
-    })
+    }
 
-
+    axios
+      .post(unifiAuthURL, secondAuthData, DEFAULT_HEADER)
+      .then(({ data }) => {
+        if (data.VUID) {
+          res.view('./src/m3u8/unifi-tv.ejs', { vuid: data.VUID})
+        } else {
+          console.error('Response from unifi server:', data)
+          res.send({ message: 'Something went wrong. Please check your credential again'}, 401)
+        }
+      })
   })
 
   fastify.get('/epg/unifi-tv', (req, res) => {
